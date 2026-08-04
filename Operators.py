b9020103f0a78ee33e455297fd9cb11c729db15f
MD. Liton Islam <fakeid25892@gmail.com>
2026-08-04 16:13:47 +0600

    Day 1

---
+++ Operators.py
@@ -0,0 +1,49 @@
+#irrimatic operators
+
+x=10
+y=5
+
+print("sum is" ,x+y)
+print("diff is",x-y)
+print("multiple is",x*y)
+print("division is",x/y)
+print("modules is",x%y)
+print("power is",x**y)
+
+#Comparision operators
+
+print("equale is",x==y) #false
+
+print("equale or gater than is",x>=y) #true
+
+print("less than is", x<y) #false
+print("gater than is" ,x>y) #true
+
+#Logical operators
+
+print("result of and operator is",x==y and x>=y) #false
+
+print("result of or operator is",x>=y or x==y) #true
+
+print("result of not operator is",not(x>y))
+
+#Assignment operator
+
+a=2
+b=3
+
+#logic and shortcurt#
+
+a=a+1
+a+=1
+
+
+a=a-1
+a-=1
+
+a=a*1
+a*=1
+
+a=a/1
+a/=1
+
