819e0ec06316411de12a5cd14379ccf3142ba207
MD. Liton Islam <fakeid25892@gmail.com>
2026-08-04 16:16:18 +0600

    Day 2

---
+++ Practice 1.py
@@ -0,0 +1,13 @@
+#question on slicing
+#take input and print middle 3 characters ,last two characters
+
+
+str=input("Enter the value :")
+mid= len(str)//2
+output1=str[mid-1:mid+2]
+print(output1)
+
+output2=str[-2:]
+print(output2)
+
+print()
---
+++ StringBasic.py
@@ -0,0 +1,15 @@
+str1='Hello'
+str2="Liton"
+str3='''Python course by Liton Islam'''
+
+print(str1)
+print(str2)
+print(str3)
+
+#String Concatenation
+
+print(str1+" "+str2)
+
+#Length of Python
+
+print(len(str2))
